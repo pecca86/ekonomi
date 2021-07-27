1f1463a8b74b5f2820ba61ea8d804482b6d16853
Pekka Ranta-aho <pekka.rantaaho86@gmail.com>
2021-07-27 21:28:46 +0300

    Production ready

--- app.js
+++ app.js
@@ -90,6 +90,17 @@ app.use("/api/v1/transactioncategories", transactionCategories);
 const errorHandler = require("./middleware/error");
 app.use(errorHandler);
 
+// PRODUCTION
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 // === Start server ===
 
 const PORT = process.env.PORT || 8080;
